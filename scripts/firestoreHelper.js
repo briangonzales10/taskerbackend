9b545ab8b2bdd255d433717575ce959892e61106
briangonzales10 <brian.gonzales10@gmail.com>
2022-06-24 08:55:31 -0400

    fixed formatting

--- scripts/firestoreHelper.js
+++ scripts/firestoreHelper.js
@@ -1,7 +1,6 @@
 const serviceAccount = require("../google-credentials.json");
 const fs = require("firebase-admin");
 
-
 const {
     initializeApp,
     applicationDefault,
@@ -15,50 +14,51 @@ const {
 
   //Open DB connection
 fs.initializeApp({
-    credential: fs.credential.cert(serviceAccount),
-    storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
-    databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`
+  credential: fs.credential.cert(serviceAccount),
+  storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
+  databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`
+});
+
+const db = fs.firestore();
+const bucket = fs.storage().bucket();
+const tasklist = tasklistCollection = db.collection("tasklist");
+const users = usersCollection = db.collection('users');
+
+let uploadFile = async (taskId, file) => {
+  console.log("starting upload")
+
+  const results = {
+    status: '',
+    message: ''
+  }
+  const fileName = file.originalname
+
+  const blob = bucket.file('proof/' + fileName)
+  const blobWriter = blob.createWriteStream({
+      metadata: {
+          contentType: file.mimetype,
+          metadata: { 'proof': taskId }
+      }
   });
-  const db = fs.firestore();
-  const bucket = fs.storage().bucket();
-  const tasklist = tasklistCollection = db.collection("tasklist");
-  const users = usersCollection = db.collection('users');
-
-  let uploadFile = async (taskId, file) => {
-    console.log("starting upload")
-
-    const results = {
-      status: '',
-      message: ''
-    }
-    const fileName = file.originalname
-
-    const blob = bucket.file('proof/' + fileName)
-    const blobWriter = blob.createWriteStream({
-        metadata: {
-            contentType: file.mimetype,
-            metadata: { 'proof': taskId }
-        }
-    });
-    blobWriter.on('error', (err) => {
-      results.status(500)
-      results.message(`Could not upload file!`)
-      console.log(err)
-    });
-    blobWriter.on('finish', () => {
-      console.log('Finished uploading')
-      results.status(200)
-      results.message(`Finished uploading ${fileName}!`)
-      updateProof(taskId, fileName)
-    })
-    blobWriter.end(file.buffer, () => {
-       return results;
-    })
+  blobWriter.on('error', (err) => {
+    results.status(500)
+    results.message(`Could not upload file!`)
+    console.log(err)
+  });
+  blobWriter.on('finish', () => {
+    console.log('Finished uploading')
+    results.status(200)
+    results.message(`Finished uploading ${fileName}!`)
+    updateProof(taskId, fileName)
+  })
+  blobWriter.end(file.buffer, () => {
+      return results;
+  })
 };
 
 function updateProof(taskId, fileName) {
   console.log(`Updating Proof Filename for ${taskId}`)
-  return await fs.tasklist.doc(taskId).update({
+  return fs.tasklist.doc(taskId).update({
     proofFilename : fileName
   })
   .then((res) => {
